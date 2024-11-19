723f7b03e3619c9869a369bcb4a8ba5b4e465cd9
Himanshu Agrawal <168897738+Himanshuagrawal2003@users.noreply.github.com>
2024-11-20 03:30:47 +0530

    Add files via upload

---
+++ Internship Management Project/static/main.js
@@ -0,0 +1,50 @@
+// Setting up firebase for login
+   const firebaseApp = firebase.initializeApp({ 
+    apiKey: "AIzaSyAgNvYLkiZRkT4jpZ2fdc676e5M3HEseZ8",
+  authDomain: "login-page-2f83e.firebaseapp.com",
+  projectId: "login-page-2f83e",
+  storageBucket: "login-page-2f83e.firebasestorage.app",
+  messagingSenderId: "218776793586",
+  appId: "1:218776793586:web:8ea0512c1417682d7dbb6b"
+    });
+   const db = firebaseApp.firestore();
+   const auth = firebaseApp.auth();
+
+//Sign up ----->
+const signUp=()=>{
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    console.log(email,password);
+
+    //firebase code
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((result) => {
+      // Signed in 
+   window.location.href ="mentorRegister.html";
+   console.log(result);
+      // ...
+    })
+    .catch((error) => {
+    console.log(error.code);
+    console.log(error.message);
+      // ..
+    });
+}
+
+//Sign in Function ---->
+const signIn=()=>{
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    //firebase code
+    firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((result) => {
+    // Signed in
+    window.location.href = "mentor.html";
+    console.log(result);
+    // ...
+  })
+  .catch((error) => {
+    console.log(error.code);
+    console.log(error.message);
+  });
+}
---
+++ Internship Management Project/static/studentMentor.js
@@ -0,0 +1,18 @@
+// Example mentor data
+const mentor = {
+    name: "Jane Smith",
+    mentorId: "MNT67890",
+    department: "Information Technology",
+    age: 35,
+    email: "janesmith@example.com",
+    gender: "Female"
+  };
+  
+  // Populate the mentor info dynamically
+  document.getElementById("name").textContent = mentor.name;
+  document.getElementById("mentorId").textContent = mentor.mentorId;
+  document.getElementById("department").textContent = mentor.department;
+  document.getElementById("age").textContent = mentor.age;
+  document.getElementById("email").textContent = mentor.email;
+  document.getElementById("gender").textContent = mentor.gender;
+  
